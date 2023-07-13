4c0953b3fee06221d85c579cf5731a442a53837c
Andrew Goessling <agoessling@gmail.com>
2023-07-12 23:06:38 -0700

    Initial commit.

---
+++ reg_def.py
@@ -0,0 +1,14 @@
+'''C register definition code generation'''
+import xml.etree.ElementTree as et
+
+
+class Register:
+  pass
+
+
+def main():
+  print(et.__name__)
+
+
+if __name__ == '__main__':
+  main()
